12075917694e7222468e5b99bdfda00839cd8c72
Nosbel Rivera <nosbe.rivera@gmail.com>
2025-01-04 14:22:24 -0600

    Fixing: library dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { toast,NgxSonnerToaster } from 'ngx-sonner';
+import { toast, NgxSonnerToaster } from 'ngx-sonner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgxSonnerToaster, TranslateModule ],
+  imports: [RouterOutlet, RouterModule, NgxSonnerToaster, TranslateModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor(private translate: TranslateService) {
-
-  }
+  constructor(private translate: TranslateService) {}
   title = 'mck-frontend';
 
   isDarkMode = false; // Variable para rastrear el estado del modo oscuro
--- src/app/features/main/main.component.ts
+++ src/app/features/main/main.component.ts
@@ -1,32 +1,46 @@
-import { BreakpointObserver, Breakpoints, MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  BreakpointObserver,
+  Breakpoints,
+  MediaMatcher,
+} from '@angular/cdk/layout';
+import {
+  ChangeDetectorRef,
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { NavItem } from '@models/request';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar'
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatMenuModule } from '@angular/material/menu';
+import { NgIf, CommonModule } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from '@services/auth/auth.service';
 import { map, Observable, Subscription } from 'rxjs';
-import { MenuListItemComponent } from "../menu-list-item/menu-list-item.component";
+import { MenuListItemComponent } from '../menu-list-item/menu-list-item.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [
     MatSidenavModule,
+    RouterModule,
     MatToolbarModule,
     MatListModule,
     MenuListItemComponent,
     MatIconModule,
     MatChipsModule,
-    MatMenuModule
-],
+    MatMenuModule,
+    NgIf,
+    CommonModule,
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
 export class MainComponent implements OnInit, OnDestroy {
   _authService = inject(AuthService);
@@ -40,22 +54,22 @@ export class MainComponent implements OnInit, OnDestroy {
     {
       icon: 'search',
       route: '/app/searches',
-      title: 'Searches'
+      title: 'Searches',
     },
     {
       icon: 'mark_email_read',
       route: '/app/validate-emails',
-      title: 'Validate'
+      title: 'Validate',
     },
     {
       icon: 'people',
       route: '/app/contacts',
-      title: 'Contacts'
+      title: 'Contacts',
     },
     {
       icon: 'send',
       route: '/app/campaigns',
-      title: 'Campaigns'
+      title: 'Campaigns',
     },
     {
       icon: 'settings',
@@ -70,8 +84,8 @@ export class MainComponent implements OnInit, OnDestroy {
           icon: 'account_circle',
           route: '/app/setting/clients',
           title: 'Clients',
-        }
-      ]
+        },
+      ],
     },
     {
       icon: 'info',
@@ -86,21 +100,21 @@ export class MainComponent implements OnInit, OnDestroy {
           icon: 'description',
           route: '/privacy-policy',
           title: 'Privacy Policy',
-        }
-      ]
-    }
+        },
+      ],
+    },
   ];
 
   myUserRoutes = [
     {
       icon: 'search',
       route: '/app/searches',
-      title: 'Searches'
+      title: 'Searches',
     },
     {
       icon: 'mark_email_read',
       route: '/app/validate-emails',
-      title: 'Validate'
+      title: 'Validate',
     },
     {
       icon: 'info',
@@ -115,18 +129,16 @@ export class MainComponent implements OnInit, OnDestroy {
           icon: 'description',
           route: '/privacy-policy',
           title: 'Privacy Policy',
-        }
-      ]
-    }
+        },
+      ],
+    },
   ];
 
   private _mobileQueryListener: () => void;
 
-
-
   isHandset$: Observable<boolean> = this._breakpointObserver
     .observe(Breakpoints.Handset)
-    .pipe(map(result => result.matches));
+    .pipe(map((result) => result.matches));
 
   constructor(
     changeDetectorRef: ChangeDetectorRef,
@@ -139,13 +151,10 @@ export class MainComponent implements OnInit, OnDestroy {
     this.mobileQuery.addListener(this._mobileQueryListener);
     translate.setDefaultLang('en');
   }
-   ngOnInit() {
-
-    this._authService.$user.subscribe(user => {
-      if (user.displayName)
-        this.userName = user.displayName;
-      else
-        this.userName = user.email?.split('@')[0]!;
+  ngOnInit() {
+    this._authService.$user.subscribe((user) => {
+      if (user.displayName) this.userName = user.displayName;
+      else this.userName = user.email?.split('@')[0]!;
       console.log(this.userName);
     });
     /* if (this.authService.isAdmin()) {
@@ -161,17 +170,15 @@ export class MainComponent implements OnInit, OnDestroy {
     .catch((err: any) => {
       console.log({ err });
     }); */
-
   }
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
-  async signOut(){
+  async signOut() {
     try {
       await this._authService.signOut();
       this._router.navigateByUrl('/auth');
-
     } catch (error) {
       console.log(error);
     }
@@ -180,4 +187,3 @@ export class MainComponent implements OnInit, OnDestroy {
     this.router.navigate(['/app/profile']);
   }
 }
-
